7589ddf5bd29b0ed6bae85a04a3e168ac01060ba
Alex Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2022-02-06 00:19:10 +0300

    Оставил каммент напоминалку
    MaterialApp для android
    Cupertino для разработки под ios

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      //Cupertino для разработки под ios
       home: Scaffold(
         backgroundColor: Colors.indigo,
         appBar: AppBar(
