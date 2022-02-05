e8499f5656c4713ce5bdc4a0e3a4df9e77311eb6
Alex Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2022-02-05 22:23:35 +0300

    Add materialApp
    Реализация виджета материал апп

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Center(
+  runApp(const MaterialApp(
+      home: Center(
     child: Text(
       "ПРИВЕТ",
       style: TextStyle(
           fontSize: 50, color: Colors.amber, fontWeight: FontWeight.bold),
       textDirection: TextDirection.ltr,
     ),
-  ));
+  )));
 }
