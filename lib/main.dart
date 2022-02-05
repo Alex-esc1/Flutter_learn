22543f0481c4bdd44472aa127cbd2decbdc30bc1
Alex Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2022-02-05 22:15:24 +0300

    Добавил недостающие файлы
    На данный момент, выведен текст Привет по центру мобилки, используется размер шрифта + цвет + жирность текста

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Center(
+    child: Text(
+      "ПРИВЕТ",
+      style: TextStyle(
+          fontSize: 50, color: Colors.amber, fontWeight: FontWeight.bold),
+      textDirection: TextDirection.ltr,
+    ),
+  ));
+}
