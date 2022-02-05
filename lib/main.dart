161bc1143f3cb04b29c87f85e27e8db37900afd3
Alex Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2022-02-06 01:17:17 +0300

    StatefulWidget
    StatefulWidget - написание динамического виджета

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
-// ignore_for_file: avoid_unnecessary_containers
+// ignore_for_file: avoid_unnecessary_containers, override_on_non_overriding_member
 
 import 'package:flutter/material.dart';
 
 void main() => runApp(My());
 
-class My extends StatelessWidget {
+class My extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyApp();
+  }
+}
+
+class _MyApp extends State<My> {
+  late bool _load;
+  late double _progress;
+
+  @override
+  void initState() {
+    _load = false;
+    _progress = 0.0;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,24 +34,32 @@ class My extends StatelessWidget {
         body: Center(
           child: Container(
             padding: const EdgeInsets.all(16),
-            child: Column(mainAxisAlignment: MainAxisAlignment.center,
-                // ignore: prefer_const_literals_to_create_immutables
-                children: <Widget>[
-                  const LinearProgressIndicator(value: 23),
-                  const Text(
-                    '23 %',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                  const Text(
+            child: _load
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: <Widget>[
+                      const LinearProgressIndicator(value: 23),
+                      Text(
+                        '${(_progress * 100).round()}%',
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ],
+                  )
+                : const Text(
                     'Press button to download',
                     style: TextStyle(color: Colors.white, fontSize: 20),
                   ),
-                ]),
           ),
         ),
-        floatingActionButton: const FloatingActionButton(
-          onPressed: null,
-          child: Icon(Icons.cloud_download),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _load = !_load;
+            });
+          },
+          child: const Icon(Icons.cloud_download),
         ),
       ),
     );
