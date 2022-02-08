e6919bf893c74c333995f845137020325298ecf8
Alex Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2022-02-08 11:55:47 +0300

    Добавил иконку

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
               const Image(
                 image: AssetImage('assets/image/bg.jpg'),
               ),
+              Image.asset('assets/icons/icon.png')
             ],
           ),
         ),
